028239052a52070f47d00b4147b6d499acb1cbf3
RHShoumik <rhshoumik12@gmail.com>
2022-10-20 05:53:44 +0600

    Pagination Done

--- src/components/ProfileCard/Profiles.jsx
+++ src/components/ProfileCard/Profiles.jsx
@@ -1,25 +1,39 @@
 import { Box, Grid, Pagination, Stack } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 import ProfileCard from './index';
+import usePagination from "./Pagination";
 
 
 const Profiles = ({ users }) => {
+  let [page, setPage] = useState(1);
+  const PER_PAGE = 6;
 
+  const count = Math.ceil(users.length / PER_PAGE);
+  const _DATA = usePagination(users, PER_PAGE);
 
+  const handleChange = (e, p) => {
+    setPage(p);
+    _DATA.jump(p);
+  };
 
   return (
     <>
       <Box sx={{ flexGrow: 1 }} padding={{ xs: "10px 20px", md: "15px 130px" }}>
         <Grid container spacing={{ xs: 4, md: 4 }} columns={{ xs: 1, md: 9 }}>
-          {users.map((user) => (
+          {_DATA.currentData().map((user) => (
             <Grid item xs={1} md={3} key={user.id}>
               <ProfileCard info={user} />
             </Grid>
           ))}
         </Grid>
       </Box>
-      <Stack sx={{alignItems:"center", p : "10px 0px 50px 0px"}} >
-        <Pagination count={10} color="primary" />
+      <Stack sx={{ alignItems: "center", p: "10px 0px 50px 0px" }}>
+        <Pagination
+          count={count}
+          page={page}
+          onChange={handleChange}
+          color="primary"
+        />
       </Stack>
     </>
   );
